f3d2e19ebda223f245e2d70d4d01289e09f41dc5
AndersonSantosAraujo <anderson.dos.santos.araujo@hotmail.com>
2023-08-30 01:07:04 -0300

    feat: add other routes on controller

--- src/controllers/product-controller.js
+++ src/controllers/product-controller.js
@@ -75,8 +75,24 @@ exports.post = (req, res) => {
 };
 
 exports.put = (req, res) => {
-  const id = req.params.id;
-  res.status(201).send({ id: id, item: req.body });
+  // const id = req.params.id;
+  // res.status(201).send({ id: id, item: req.body });
+  Product.findByIdAndUpdate(req.params.id, {
+    $set: {
+      title: req.body.title,
+      description: req.body.description,
+      price: req.body.price,
+      slug: req.body.slug,
+    },
+  })
+    .then((x) => {
+      res.status(200).send({ message: "Produto atualizado com sucesso!" });
+    })
+    .catch((e) => {
+      res
+        .status(400)
+        .send({ message: "Falha ao atualizar o produto!", data: e });
+    });
 };
 
 exports.delete = (req, res) => {
