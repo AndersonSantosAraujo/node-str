364ff4e08bccdb2ac3b6053cf16dee2831a823af
AndersonSantosAraujo <anderson.dos.santos.araujo@hotmail.com>
2023-09-01 21:43:49 -0300

    feat: authentication implement

--- src/routes/order-route.js
+++ src/routes/order-route.js
@@ -1,10 +1,12 @@
 "use strict";
 
+const authService = require("../services/auth-service");
+
 const express = require("express");
 const router = express.Router();
 const controller = require("../controllers/order-controller");
 
-router.get("/", controller.get);
-router.post("/", controller.post);
+router.get("/", authService.authorize, controller.get);
+router.post("/", authService.authorize, controller.post);
 
 module.exports = router;
